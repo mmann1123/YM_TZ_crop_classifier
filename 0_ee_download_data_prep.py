028fa293559f33b079853b4dac6467a900a384ec
mmann1123 <mmann1123@gmail.com>
2023-07-25 10:59:49 -0400

    expand region to northern TZ

--- 0_ee_download_data_prep.py
+++ 0_ee_download_data_prep.py
@@ -6,6 +6,8 @@
 
 import pendulum
 import ee
+import os
+import geemap
 
 from helpers import *
 from ipygee import *
@@ -24,7 +26,7 @@ crs = "EPSG:32736"
 # ## Get image bounds
 bbox = (
     gpd.read_file(
-        r"/home/mmann1123/extra_space/Dropbox/Tanzania_data/Projects/YM_Tanzania_Field_Boundaries/Land_Cover/data/bounds.gpkg",
+        r"/home/mmann1123/extra_space/Dropbox/Tanzania_data/bounds/northern_TZ_states.geojson",
     )
     .to_crs("epsg:4326")
     .total_bounds
@@ -41,7 +43,7 @@ bands = ["B2", "B3", "B4", "B8"]
 scale = 10
 # date_pattern = "mm_dd_yyyy"  # dd: day, MMM: month (JAN), y: year
 folder = "Tanzania_Fields"
-
+out_dir = os.path.expanduser("~/Downloads")
 
 # extra = dict(sat="Sen_TOA")
 CLOUD_FILTER = 75
@@ -61,9 +63,19 @@ def addEVI(image):
 
 
 # %% MONTHLY COMPOSITES
+os.chdir(
+    r"/home/mmann1123/extra_space/Dropbox/Tanzania_data/Projects/YM_Tanzania_Field_Boundaries/Land_Cover/northern_TZ_data"
+)
 
-for year in list(range(2022, 2024)):
-    for month in list(range(1, 13, 1)):
+# fishnet = geemap.fishnet(
+#     data=site,
+#     rows=15,
+#     cols=15,
+#     delta=0,
+# )
+
+for year in list(range(2023, 2024)):
+    for month in list(range(1, 8, 1))[-1:]:
         print("year ", str(year), " month ", str(month))
         dt = pendulum.datetime(year, month, 1)
 
@@ -85,7 +97,7 @@ for year in list(range(2022, 2024)):
             .clip(site)
             .unmask(9999)
         )
-        s2_sr = geetools.batch.utils.convertDataType("uint32")(s2_sr)
+        s2_sr = geetools.batch.utils.convertDataType("int16")(s2_sr)
         # eprint(s2_sr)
 
         # # export clipped result in Tiff
@@ -93,7 +105,7 @@ for year in list(range(2022, 2024)):
         img_name = "S2_SR_EVI_M_" + str(year) + "_" + str(month).zfill(2)
         export_config = {
             "scale": scale,
-            "maxPixels": 5000000000,
+            "maxPixels": 9000000000,
             "driveFolder": folder,
             "region": site,
             "crs": crs,
@@ -101,11 +113,29 @@ for year in list(range(2022, 2024)):
         task = ee.batch.Export.image(s2_sr, img_name, export_config)
         task.start()
 
+    # geemap.download_ee_image_tiles(
+    #     image=s2_sr,
+    #     features=fishnet,
+    #     out_dir=os.getcwd(),
+    #     prefix=img_name,
+    #     crs=crs,
+    #     scale=scale,
+    #     resampling="near",
+    #     dtype="int16",
+    #     overwrite=True,
+    #     max_tile_size=None,
+    #     max_tile_dim=None,
+    #     shape=None,
+    #     scale_offset=False,
+    #     unmask_value=9999,
+    # )
+
+
 ############################################################################################################
 # %% SWIR and Red Edge
 
-# Set parameters
-bands = ["B2", "B5", "B6", "B11", "B12"]
+# Set parameters ("B5" not important in mini-study)
+bands = ["B2", "B6", "B11", "B12"]
 scales = [10, 20, 20, 20, 20]
 # date_pattern = "mm_dd_yyyy"  # dd: day, MMM: month (JAN), y: year
 folder = "Tanzania_Fields"
@@ -117,8 +147,8 @@ CLOUD_FILTER = 75
 
 # %% MONTHLY COMPOSITES
 
-for year in list(range(2022, 2024)):
-    for month in list(range(1, 13, 1)):
+for year in list(range(2023, 2024)):
+    for month in list(range(1, 8, 1)):
         print("year ", str(year), " month ", str(month))
         dt = pendulum.datetime(year, month, 1)
 
@@ -138,14 +168,14 @@ for year in list(range(2022, 2024)):
                 .clip(site)
                 .unmask(9999)
             )
-            s2_sr = geetools.batch.utils.convertDataType("uint32")(s2_sr)
+            s2_sr = geetools.batch.utils.convertDataType("int16")(s2_sr)
 
             # # export clipped result in Tiff
 
             img_name = "S2_SR_" + band + "_M_" + str(year) + "_" + str(month).zfill(2)
             export_config = {
                 "scale": scale,
-                "maxPixels": 5000000000,
+                "maxPixels": 9000000000,
                 "driveFolder": folder,
                 "region": site,
                 "crs": crs,
